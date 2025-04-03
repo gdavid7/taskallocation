1f8d0379868999b290291db0863f5aeb5f658222
David Gershony <david.gershony.7@gmail.com>
2025-04-03 09:57:28 -0700

    bruh

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from database import connect_to_db
+import models.codebert_model
 
+
+
+
+# Load the JSON data into a dict object
+
+# connecting to the database, creating the json file
 connect_to_db()
+
+
