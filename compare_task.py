98ccceb8d577249f396224eca8c050685bd46ab5
Dustin Lee <dustidl1@uci.edu>
2025-04-09 16:25:01 -0700

    previous compare_task for only single, now renamed to compare_single_task. in addition, we can compare_all_task wiht new function

--- compare_task.py
+++ compare_task.py
@@ -40,7 +40,7 @@ def get_user_ranking(actual_user, ranked_user_ids: list) -> tuple:
 
 
 # compares a SINGLE task against each user to find average scores for each one
-def compare_task(model, testing_embed, database_embeds:dict) -> dict:
+def compare_single_task(model, testing_embed, database_embeds:dict) -> dict:
     # ex: comparing test task 1 against everything. issues_test is an embed
     results = {}
         # Now we have our embed that we want to compare against all the other ones
@@ -49,4 +49,14 @@ def compare_task(model, testing_embed, database_embeds:dict) -> dict:
         user_avg_similarity = compute_average_similarity(model, testing_embed, user_embeds)
         # store the similarity score for the user
         results[user_ID] = user_avg_similarity
+    return results
+
+# O(n^2)
+def compare_all_task(model, testing_embeds, database_embeds:dict) -> dict:
+    results = {}
+    # go through all the other testing since we only did it once
+    for task_id, testing_embed in testing_embeds.items():
+        task_results = compare_single_task(model, testing_embed, database_embeds)
+        results[task_id] = task_results
+
     return results
