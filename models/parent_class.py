57fae9e360711a07548732d8402bc1700c9e66a3
Dustin Lee <dustidl1@uci.edu>
2025-04-09 17:27:58 -0700

    created the convert function so we can start with embeddings in the beginnning

--- models/parent_class.py
+++ models/parent_class.py
@@ -1,9 +1,20 @@
 from abc import ABC, abstractmethod
 class Model(ABC):
-    def compare(self, task1, task2, weight_desc = 0.6, weight_code = 0.4):
-        pass
     def __init__(self, name):
         self._name = name
+
     def name(self):
         return self._name
     
+    def get_embedding(self, text):
+        pass
+    
+    def convert(self, issue: dict, model) -> list:
+        description_embed = model.get_embedding(issue["description"])
+        # if issue.get("code"):
+        #     code_embed = model.get_embedding(issue["code"])
+        #     return [description_embed, code_embed]
+        return description_embed
+
+    def compare(self, task1, task2, weight_desc = 0.6, weight_code = 0.4):
+        pass
